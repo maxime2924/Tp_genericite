6da14d6f6b20aed45446cae99cdb6e3577211274
maxime le berre <60474520+maxime2924@users.noreply.github.com>
2025-12-15 17:21:15 +0100

    Add Livre class extending Media

---
+++ Livre.java
@@ -0,0 +1,19 @@
+public class Livre extends Media {
+    private String auteur;
+    private int nbPages;
+
+    public Livre(String titre, int anneePublication, String auteur, int nbPages) {
+        super(titre, anneePublication);
+        this.auteur = auteur;
+        this.nbPages = nbPages;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Livre de " + auteur + ", " + nbPages + " pages.";
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+}
