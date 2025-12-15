0cb0a19ce1a6354e9d2d11cd90ae93919da67140
maxime le berre <60474520+maxime2924@users.noreply.github.com>
2025-12-15 17:19:58 +0100

    Create abstract Media class with properties and methods

---
+++ Media.java
@@ -0,0 +1,33 @@
+public abstract class Media {
+    private String titre;
+    private int anneePublication;
+
+    public Media() {
+        this.titre = "";
+        this.anneePublication = 0;
+    }
+
+    public Media(String titre, int anneePublication) {
+        this.titre = titre;
+        this.anneePublication = anneePublication;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public int getAnneePublication() {
+        return anneePublication;
+    }
+
+    public abstract String getDescription();
+
+    @Override
+    public String toString() {
+        return titre + " (" + anneePublication + ")";
+    }
+
+    public void afficherDetails() {
+        System.out.println(getDescription());
+    }
+}
